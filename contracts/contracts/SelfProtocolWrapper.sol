d8cf986b8f74c0fd8fb0e8f7f3b5972287e7c6d9
MikkySnow <mikkysnowman@gmail.com>
2025-07-21 15:01:17 +0300

    chore: updating wrapper contract

--- contracts/contracts/SelfProtocolWrapper.sol
+++ contracts/contracts/SelfProtocolWrapper.sol
@@ -7,14 +7,15 @@ import {SelfStructs} from "@selfxyz/contracts/contracts/libraries/SelfStructs.so
 import {AttestationId} from "@selfxyz/contracts/contracts/constants/AttestationId.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+import {UserDefinedDataLib} from "./UserDefinedDataLib.sol";
+
 contract SelfProtocolWrapper is SelfVerificationRoot {
     bool public isInRecoveryMode;
-    address public proposedAddress;
 
-    uint256 public nullifier;
+    uint256 public masterNullifier;
+
+    address public masterSigner;
     address public allowedSigner;
-
-    address public master;
 
     event NullifierSet(uint256 nullifier);
     event AllowedSigner(address allowedSigner);
@@ -25,7 +26,7 @@ contract SelfProtocolWrapper is SelfVerificationRoot {
     constructor(
         uint256 _scope // Application-specific scope identifier,
     ) SelfVerificationRoot(0x68c931C9a534D37aa78094877F46fE46a49F1A51, _scope) {
-        master = msg.sender;
+        masterSigner = msg.sender;
     }
 
     function enableRecoveryMode() public {
@@ -36,11 +37,6 @@ contract SelfProtocolWrapper is SelfVerificationRoot {
     function disableRecoveryMode() public {
         isInRecoveryMode = false;
         emit RecoveryModeDisabled();
-    }
-
-    function proposeRecovery(address _proposedAddress) public {
-        proposedAddress = _proposedAddress;
-        isInRecoveryMode = true;
     }
 
     function getConfigId(
@@ -58,17 +54,19 @@ contract SelfProtocolWrapper is SelfVerificationRoot {
         if (!isInRecoveryMode) {
             // Nullifier can be set only for master
             address recoveredAddress = address(uint160(output.userIdentifier));
-            require(recoveredAddress == master, "Only master can set nullifier");
+            require(recoveredAddress == masterSigner, "Only master can set nullifier");
 
-            nullifier = output.nullifier;
+            masterNullifier = output.nullifier;
 
-            emit NullifierSet(nullifier);
+            emit NullifierSet(masterNullifier);
         } else {
             // Otherwise we are in recovery mode. If proposed address can provide a valid proof,
             // we can set the new allowed signer
-            require(nullifier == output.nullifier, "Wrong nullifier provided");
+            require(masterNullifier == output.nullifier, "Wrong nullifier provided");
 
-            allowedSigner = proposedAddress;
+            address parsedAddress = UserDefinedDataLib.bytesToAddress(userData);
+
+            allowedSigner = parsedAddress;
             isInRecoveryMode = false;
 
             emit AllowedSigner(allowedSigner);
