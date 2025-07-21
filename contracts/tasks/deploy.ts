1412b28545302a3b6734fea3c37c44c9c14a2c35
MikkySnow <mikkysnowman@gmail.com>
2025-07-21 15:07:25 +0300

    feat: add deploy task

---
+++ contracts/tasks/deploy.ts
@@ -0,0 +1,26 @@
+import {task} from "hardhat/config";
+import {HardhatRuntimeEnvironment} from "hardhat/types";
+import {calculateContractAddress, hashEndpointWithScope} from "./scopeUtils";
+
+task("deploy", "Deploy SelfProtocolAccount contract")
+    .setAction(async (taskArgs, hre: HardhatRuntimeEnvironment) => {
+        // Calculate contract address to be deployed
+        const [walletClient] = await hre.viem.getWalletClients()
+        const scopeSeed = "my-app-dev"
+        const calculatedContractAddress = await calculateContractAddress(walletClient.account.address)
+        console.log("Calculated contract address", calculatedContractAddress)
+        const scope = hashEndpointWithScope(calculatedContractAddress, scopeSeed)
+        console.log("Scope", scope)
+
+        // Get the contract factory
+        const contract = await hre.viem.deployContract("SelfDebugUserDefinedData", [
+            BigInt(scope)
+        ]);
+
+        console.log("Deployed contract address:", contract.address);
+
+        return {
+            address: contract.address,
+        };
+    });
+
